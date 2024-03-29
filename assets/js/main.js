c8a80e37f5fee723c44288446dafddeb0759c3aa
Sarwar Husain <sarwar.info2000@gmail.com>
2024-03-29 15:50:35 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove('skills__active')
+            })
+
+            target.classList.add('skills__active')
+
+            tabs.forEach(tab => {
+                tab.classList.remove('skills__active')
+            })
+
+            tab.classList.add('skills__active')
+        })
+    })
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(L=> L.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(L=> L.addEventListener("click", activeWork))
+/*===== Work Popup =====*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== INPUT ANIMATION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
